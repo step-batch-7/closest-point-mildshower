cd5847b2c1051e220530424d64bda41d2b9da9c3
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-04-13 15:38:26 +0530

    added given test data in main function

--- main.c
+++ main.c
@@ -3,8 +3,14 @@
 
 int main(void)
 {
-  point food_points[2] = {{1, 3}, {2, 2}}, closest_food_location, curr_location = {0, 0};
-  get_closest_food(food_points, 2, curr_location, &closest_food_location);
-  printf("x: %d, y:%d", closest_food_location.x, closest_food_location.y);
+  point food_points[5] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}}, closest_food_location;
+  point curr_locations[5] = {{18, 86}, {97, 27}, {69, 7}, {10, 94}, {36, 27}};
+
+  for (int index = 0; index < 5; index++)
+  {
+    get_closest_food(food_points, 5, curr_locations[index], &closest_food_location);
+    printf("Location of organism: [%d %d],  Closest food target : [%d %d]\n", curr_locations[index].x, curr_locations[index].y, closest_food_location.x, closest_food_location.y);
+  }
+
   return 0;
 }
